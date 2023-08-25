eb2db22ef6616a6bfca9627a2aec461f66e6401c
Navaneeth <navaneethkulal8@gmail.com>
2023-08-25 23:32:13 +0530

    schedule page ui improvements

--- lib/pages/busschedulepage.dart
+++ lib/pages/busschedulepage.dart
@@ -7,59 +7,76 @@ class BusSchedulePage extends StatefulWidget {
 
 class _BusSchedulePageState extends State<BusSchedulePage> {
   List<bool> bookmarks = List.generate(
-      5, (index) => false); // Replace 5 with actual number of bus schedules
+      5, (index) => false); // Replace 5 with the actual number of bus schedules
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Text(
-              'Bus Your Bookmarks',
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Bus Schedule Bookmarks',
+          style: TextStyle(color: Colors.black),
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 20),
+            Text(
+              'Your Bus Bookmarks',
+              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
-          ),
-          Expanded(
-            child: ListView.builder(
-              itemCount: bookmarks.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 10.0, vertical: 8.0),
-                  child: Card(
-                    elevation: 4,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    color: Colors.white,
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(10.0),
-                      title: Text('Bus Schedule ${index + 1}',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      subtitle: Text(
-                        'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+            SizedBox(height: 20),
+            Expanded(
+              child: ListView.builder(
+                itemCount: bookmarks.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 20.0),
+                    child: Card(
+                      elevation: 4,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(15.0),
                       ),
-                      trailing: IconButton(
-                        icon: Icon(Icons.delete),
-                        onPressed: () {
-                          // TODO: Handle deleting the bookmark
+                      color: Colors.grey[100],
+                      child: ListTile(
+                        contentPadding: EdgeInsets.all(16.0),
+                        title: Text(
+                          'Bus Schedule ${index + 1}',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18),
+                        ),
+                        subtitle: Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+                        ),
+                        trailing: IconButton(
+                          icon: Icon(Icons.delete, color: Colors.red),
+                          onPressed: () {
+                            // TODO: Handle deleting the bookmark
+                            setState(() {
+                              bookmarks.removeAt(index);
+                            });
+                          },
+                        ),
+                        onTap: () {
+                          setState(() {
+                            bookmarks[index] = !bookmarks[index];
+                          });
                         },
                       ),
-                      onTap: () {
-                        setState(() {
-                          bookmarks[index] = !bookmarks[index];
-                        });
-                      },
                     ),
-                  ),
-                );
-              },
+                  );
+                },
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
